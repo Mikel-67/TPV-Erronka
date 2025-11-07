800cf69f017d47763f9b6d8e9fbacdf438ab0b8b
Mikel-67 <mgoyago25paag@ikzubirimanteo.com>
2025-11-07 13:27:05 +0100

    Admin bista egina

--- Erronka1/Erronka1/Bistak/AdminWindow.xaml.cs
+++ Erronka1/Erronka1/Bistak/AdminWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Erronka1.Bistak.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,22 @@ namespace Erronka1.Bistak
         public AdminWindow()
         {
             InitializeComponent();
+            Zeerenda.Content = new StockView(); // Defektuzko bista
+        }
+
+        private void BtnUserrak(Object sender, RoutedEventArgs e)
+        {
+            Zeerenda.Content = new UsersView();
+        }
+        private void BtnStocka(Object sender, RoutedEventArgs e)
+        {
+            Zeerenda.Content = new StockView();
+        }
+        private void SesioaItxi(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            this.Close();
         }
     }
 }
--- Erronka1/Erronka1/obj/Debug/net8.0-windows/Bistak/ErabiltzaileWindow.g.i.cs
+++ Erronka1/Erronka1/obj/Debug/net8.0-windows/Bistak/ErabiltzaileWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace Erronka1.Bistak {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/Erronka1;V1.0.0.0;component/bistak/erabiltzailewindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/Erronka1;component/bistak/erabiltzailewindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\Bistak\ErabiltzaileWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
